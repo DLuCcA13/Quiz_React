0d652ade24b24d140ba7595dc8a54d361c6800a8
DLuCcA13 <enzzodlbs@gmail.com>
2024-05-18 21:04:27 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/*import Quiz from "./Projetos/Quiz";
+/*
+import Relogio from "./Projetos/Relogio";
+import Quiz from "./Projetos/Quiz";
 import ConversorMoedas from "./Projetos/ConversorMoedas";
 import GaleriaFotos from "./Projetos/GaleriaFotos";
 import BuscaFilmes from "./Projetos/BuscaFilme";
 import ListaContatos from "./Projetos/ListaContatos";
 import Cronometro from "./Projetos/Cronometro";
 import ListaTarefas from "./Projetos/ListaTarefas"
-import Contador from "./Projetos/Contador";*/
-import Relogio from "./Projetos/Relogio";
+*/
+import Contador from "./Projetos/Contador";
 import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Relogio/>
+    <Contador/>
   </React.StrictMode>
 );
