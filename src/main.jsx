1dcbd931b63b16c9a8c323c5ed4fd1c4efe924d7
DLuCcA13 <enzzodlbs@gmail.com>
2024-05-18 21:55:23 -0300

    firt commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 /*
+import ListaContatos from "./Projetos/ListaContatos";
 import Cronometro from "./Projetos/Cronometro";
 import  from "./Projetos/ListaTarefas";
 import Contador from "./Projetos/Contador";
@@ -9,13 +10,14 @@ import Relogio from "./Projetos/Relogio";
 import Quiz from "./Projetos/Quiz";
 import ConversorMoedas from "./Projetos/ConversorMoedas";
 import GaleriaFotos from "./Projetos/GaleriaFotos";
-import BuscaFilmes from "./Projetos/BuscaFilme";
+
 
 */
-import ListaContatos from "./Projetos/ListaContatos";
+
+import BuscaFilmes from "./Projetos/BuscaFilme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ListaContatos />
+    <BuscaFilmes />
   </React.StrictMode>
 );
