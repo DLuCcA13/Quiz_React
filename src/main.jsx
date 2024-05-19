b18962a19513ae681110a3e1fe42df108d28167f
DLuCcA13 <enzzodlbs@gmail.com>
2024-05-18 22:07:34 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 /*
+import GaleriaFotos from "./Projetos/GaleriaFotos";
 import BuscaFilmes from "./Projetos/BuscaFilme";
 import ListaContatos from "./Projetos/ListaContatos";
 import Cronometro from "./Projetos/Cronometro";
@@ -12,11 +13,11 @@ import Quiz from "./Projetos/Quiz";
 import ConversorMoedas from "./Projetos/ConversorMoedas";
 
 */
-import GaleriaFotos from "./Projetos/GaleriaFotos";
+import ConversorMoedas from "./Projetos/ConversorMoedas";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GaleriaFotos />
+    <ConversorMoedas />
   </React.StrictMode>
 );
